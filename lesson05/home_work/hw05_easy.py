a089b89319a359f434e6b054f7fea290ee8c96ff
Leo <bgdnff@gmail.com>
2018-11-12 17:51:16 +0500

    Часть изи. Не успеваю :(

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,28 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+import os
+new_dir = list('dir_{}'.format(i) for i in range(1,10))
+for d in new_dir:
+    try:
+       os.mkdir(d)
+    except FileExistsError:
+        pass # просто промолчим
+        # print(d,' уже существует')
+
+del_dir =  list('dir_{}'.format(i) for i in range(1,10))
+for d in del_dir:
+    try:
+        pass #os.rmdir(d)
+    except FileNotFoundError:
+        pass  # просто промолчим
+        # print(d,' нет такого')
+
+
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+print(list(filter(os.path.isdir, os.listdir())))
 
 # Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт
